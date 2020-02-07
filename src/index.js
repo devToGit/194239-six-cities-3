311785ca6e3d8996fb1da4aba8ab8d76750a691f
devToGit <nnos@mail.ru>
2020-02-07 21:23:29 +0300

    1.3 Личный проект: начинаем программировать

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app.jsx";
+
+const Settings = {
+  PLACES_COUNT: 314
+};
+
+ReactDOM.render(
+    <App
+      placesCount={Settings.PLACES_COUNT}
+    />,
+    document.querySelector(`#root`)
+);
 
